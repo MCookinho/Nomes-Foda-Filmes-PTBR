b757e18b22cb0e19653617ea400995a6dd03c8a5
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-03-31 18:01:46 -0300

    Update automatico.py

--- automatico.py
+++ automatico.py
@@ -8,6 +8,7 @@ api_key = os.environ['TWITTER_API_KEY']
 api_secret = os.environ['TWITTER_API_SECRET']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
 access_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+bearer_token = os.environ['BEARER_TOKEN']
 
 # V1 Twitter API Authentication
 auth = tweepy.OAuthHandler(api_key, api_secret)
