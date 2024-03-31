6841b8bb77ed5def0e0dcc843090d7c5b84898b2
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-03-31 17:59:57 -0300

    Update automatico.py

--- automatico.py
+++ automatico.py
@@ -5,7 +5,7 @@ import os
 
 #definição das chaves
 api_key = os.environ['TWITTER_API_KEY']
-api_secret = os.environ['TWITTER_API_SECRET_KEY']
+api_secret = os.environ['TWITTER_API_SECRET']
 access_token = os.environ['TWITTER_ACCESS_TOKEN']
 access_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
